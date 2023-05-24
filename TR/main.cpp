9e2909f675d2c2d21238cf7373323a121f631a8d
aruma <k022g0101@g.neec.ac.jp>
2023-05-25 06:34:20 +0900

    de

--- TR/main.cpp
+++ TR/main.cpp
@@ -21,10 +21,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	dirt4->Initialize();
 	dirt5->Initialize();
 	Vector2 spd;
-	int num2 = 0;
-	int num3 = 0;
-	int num4 = 0;
-	int num5 = 0;
+	srand((unsigned)time(NULL));
+	int num[4] = {};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -39,10 +37,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 		if (keys[DIK_SPACE] && sphere->mIsShot == false) {
-			num2 = rand() % 5;
-			num3 = rand() % 3 - 2;
-			num4 = rand() % 2 - 3;
-			num5 = rand() % -5;
+			for (int i = 0; i < 4; i++) {
+				num[i] = rand() % 5 - 5;
+			}
 		}
 		game->Update(sphere, dirt1);
 		dirt2->Update();
@@ -50,6 +47,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		dirt4->Update();
 		dirt5->Update();
 		if (sphere->mIsHit && sphere->mPosition.y!=360.0f) {
+			dirt1->Initialize();
+			dirt2->Initialize();
+			dirt3->Initialize();
+			dirt4->Initialize();
+			dirt5->Initialize();
 			dirt1->mPosition.x = sphere->mDirtPosition.x;
 			dirt1->mPosition.y = sphere->mDirtPosition.y;
 			dirt2->mPosition.x = sphere->mDirtPosition.x;
@@ -60,20 +62,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			dirt4->mPosition.y = sphere->mDirtPosition.y;
 			dirt5->mPosition.x = sphere->mDirtPosition.x;
 			dirt5->mPosition.y = sphere->mDirtPosition.y;
-			/*spd = game->GetTheta(sphere);
-			dirt->mVelocity.x = spd * 5;
-			dirt->mVelocity.y = -(sphere->mVelocity.y)/3;*/
 			spd = game->GetSpeed(sphere);
 			dirt1->mVelocity.x = spd.x;
 			dirt1->mVelocity.y = spd.y;
-			dirt2->mVelocity.x = spd.x + num2;
-			dirt2->mVelocity.y = spd.y - num2;
-			dirt3->mVelocity.x = spd.x + num3;
-			dirt3->mVelocity.y = spd.y - num3;
-			dirt4->mVelocity.x = spd.x + num4;
-			dirt4->mVelocity.y = spd.y - num4;
-			dirt5->mVelocity.x = spd.x + num5;
-			dirt5->mVelocity.y = spd.y - num5;
+				dirt2->mVelocity.x = spd.x + num[0];
+				dirt2->mVelocity.y = spd.y - num[1];
+				dirt3->mVelocity.x = spd.x - num[1];
+				dirt3->mVelocity.y = spd.y + num[2];
+				dirt4->mVelocity.x = spd.x - num[2];
+				dirt4->mVelocity.y = spd.y + num[3];
+				dirt5->mVelocity.x = spd.x + num[3];
+				dirt5->mVelocity.y = spd.y - num[0];
 		}
 		///
 		/// ↑更新処理ここまで
@@ -83,15 +82,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		game->Draw(sphere, dirt1);
-		//dirt2->mPosition.x = dirt->mPosition.x + num;
-		//dirt2->mPosition.y = dirt->mPosition.y - num;
 		dirt2->Draw();
 		dirt3->Draw();
 		dirt4->Draw();
 		dirt5->Draw();
-		Novice::ScreenPrintf(0, 0, "%f", game->GetTheta(sphere));
-		Novice::ScreenPrintf(0, 20, "%f", dirt1->mPosition.x);
-		Novice::ScreenPrintf(0, 40, "%f", dirt1->mPosition.y);
 		///
 		/// ↑描画処理ここまで
 		///
